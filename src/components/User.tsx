85960c682a86c90b06716c277895a7619d6c7811
gyanchandra moirangthem <gyanchandra moirangthem>
2021-09-29 08:08:49 +0530

    React TypeScript Tutorial - 10 - useState Future Value

---
+++ src/components/User.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+type authUser = {
+  name: string;
+  email: string;
+};
+
+const User = () => {
+  const [user, setUser] = useState<authUser|null>(null);
+
+  const handleLogin = () => {   
+    setUser(
+        {
+            name:'gyan',
+            email :'gsingh@gmail.com'
+        }
+    );
+  };
+  const handleLogout = () => {
+    setUser(null);
+  };
+
+  return (
+    <div>
+      <button onClick={handleLogin}>log in</button>
+      <button onClick={handleLogout}>log out</button>
+      <div>user name is : {user?.name} </div>
+      <div>user email is :{user?.email} </div>
+    </div>
+  );
+};
+
+export default User;
