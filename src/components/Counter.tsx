e27e12488774261faa8cd8e3f6c63c719e719185
gyanchandra moirangthem <gyanchandra moirangthem>
2021-09-29 12:34:53 +0530

    React TypeScript Tutorial - 12 - useReducer Hook

---
+++ src/components/Counter.tsx
@@ -0,0 +1,39 @@
+import React, { useReducer } from "react";
+
+type counteState = {
+  count: number;
+};
+
+type counterAction = {
+  type: string;
+  payload: number;
+};
+const initialState = { count: 0 };
+
+function reducer(state: counteState, action: counterAction) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + action.payload };
+    case "decrement":
+      return { count: state.count - action.payload };
+    default:
+      return state;
+  }
+}
+
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <>
+      count value : {state.count}
+      <button onClick={() => dispatch({ type: "increment", payload: 10 })}>
+        increment by 10
+      </button>
+      <button onClick={() => dispatch({ type: "increment", payload: 10 })}>
+        decrement by 10
+      </button>
+    </>
+  );
+};
+
+export default Counter;
